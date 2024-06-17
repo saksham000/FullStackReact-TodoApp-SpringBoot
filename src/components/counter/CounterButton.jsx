231fccbfbcc2831e9b9d0a55d847cc2daaa0e628
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-06-18 00:24:55 +0530

    syntax update

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -1,25 +1,25 @@
 import { PropTypes } from "prop-types";
 
-export default function CounterButton({ by, incrementMethod, decrementMethod}) {
-  
-  
-  
-    return (
-      <div className="Counter">
-        <div>
-          <button className="counterButton" onClick={()=>incrementMethod(by)}>
-            +{by}
-          </button>
-  
-          <button className="counterButton" onClick={()=>decrementMethod(by)}>
-            -{by}
-          </button>
+export default function CounterButton({
+  by,
+  incrementMethod,
+  decrementMethod,
+}) {
+  return (
+    <div className="Counter">
+      <div>
+        <button className="counterButton" onClick={() => incrementMethod(by)}>
+          +{by}
+        </button>
 
-        </div>
+        <button className="counterButton" onClick={() => decrementMethod(by)}>
+          -{by}
+        </button>
       </div>
-    );
-  }
-  
-  CounterButton.propTypes = {
-    by: PropTypes.number,
-  };
+    </div>
+  );
+}
+
+CounterButton.propTypes = {
+  by: PropTypes.number,
+};
